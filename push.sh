1a63fb327d6c293c03d2e3e4da42e3a2ca6029d1
Jozef Reisinger <jozef.reisinger@gmail.com>
2017-01-02 21:19:02 +0100

    minor change

--- push.sh
+++ push.sh
@@ -12,7 +12,8 @@ commit_website_files() {
 
 upload_files() {
   git remote add origin https://${GH_TOKEN}@github.com/jreisinger/should-do.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin
+  #git push --quiet --set-upstream origin
+  git push origin master
 }
 
 setup_git
