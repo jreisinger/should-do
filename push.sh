33148ebe184d367f4082b8e5308510c5a896bd2f
Jozef Reisinger <jozef.reisinger@gmail.com>
2017-01-02 20:56:09 +0100

    add travis stuff

---
+++ push.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git commit -am "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+  git remote add origin https://${GH_TOKEN}@github.com/jreisinger/should-do.git > /dev/null 2>&1
+  git push --quiet --set-upstream origin
+}
+
+setup_git
+commit_website_files
+upload_files
+
